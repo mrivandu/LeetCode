eb670a2dcad977c2ac8ec3d176e2df28818bd83b
mrivandu <mrivandu@hotmail.com>
2019-05-03 20:05:52 +0800

    Update

---
+++ Gitlab/gitlab_install_configurate.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#===============================================================================
+#          FILE: gitlab_install_configurate.sh
+#         USAGE: . ${YOUR_PATH}/gitlab_install_configurate.sh 
+#   DESCRIPTION: 
+#        AUTHOR: IVAN DU
+#        E-MAIL: mrivandu@hotmail.com
+#        WECHAT: ecsboy
+#      TECHBLOG: https://ivandu.blog.csdn.net
+#        GITHUB: https://github.com/mrivandu
+#       CREATED: 2019-05-03 19:52:44
+#       LICENSE: GNU General Public License.
+#     COPYRIGHT: © IVAN DU 2019
+#      REVISION: v1.0
+#===============================================================================
+
+# Stop and disable Firewalld.
+systemctl stop firewalld
+systemctl disable firewalld
+
+# Disable SELinux.
+
+# Omnibus GitLab-ce package
+yum -y install curl policycoreutils openssh-server openssh-client postfix
